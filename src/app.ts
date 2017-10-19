0483850355e0b7a7c31b32b942ff395625fe8774
LongYinan <lynweklm@gmail.com>
2017-10-19 15:10:38 +0800

    init

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import { interval } from 'rxjs/observable/interval'
+import { take, skip, switchMap, finalize, map } from 'rxjs/operators'
+
+const streamA$ = interval(1000)
+.pipe(
+  map(c => `From A: ${ c }`)
+)
+const streamB$ = interval(200)
+  .pipe(
+    map(c => `From B: ${ c }`),
+    finalize(() => {
+      console.log('I\'m done')
+    })
+  )
+
+streamA$
+  .pipe(
+    skip(2),
+    take(10),
+    switchMap(() => streamB$)
+  )
+  .subscribe((r) => {
+    console.log(r)
+  })
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const { resolve } = require('path')
+const webpack = require('webpack')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const tsImportPluginFactory = require('ts-import-plugin')
+
+const importPlugin = tsImportPluginFactory({
+  libraryName: 'rxjs/operators',
+  style: false,
+  libraryDirectory: '',
+  camel2DashComponentName: false,
+  transformToDefaultImport: false
+})
+
+module.exports = {
+  entry: {
+    app: './src/app.ts'
+  },
+  output: {
+    filename: '[name].js',
+    path: resolve(process.cwd(), 'dist')
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|tsx|js|ts)$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+          getCustomTransformers: () => ({
+            before: [ importPlugin ]
+          })
+        },
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+
+    new webpack.HashedModuleIdsPlugin(),
+
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: {
+        screw_ie8: true
+      },
+      compress: {
+        screw_ie8: true,
+        dead_code: true,
+        warnings: false
+      },
+      beautify: false,
+      sourceMap: false,
+      comments: false
+    }),
+
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      reportFilename: './report.html'
+    })
+  ]
+}
